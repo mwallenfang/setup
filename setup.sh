35d2a90533fbce54f1098a2c9563d7bb411391d5
mwallenfang <marvin@wallenfang.de>
2023-10-10 21:20:05 +0200

    Rust and software

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 # install packages
-pacman -Syu base-devel networkmanager awesome lightdm lightdm-slik-greeter alacritty xterm git pipewire lib32-pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack firefox wireplumber code discord dmenu
+
+# dev packages
+pacman -Syu base-devel git python
+
+# system basics
+pacman -Syu networkmanager pipewire lib32-pipewire pipewire-audio pipewire-alsa \ 
+pipewire-pulse pipewire-jack
+
+# UI
+pacman -Syu awesome lightdm lightdm-slik-greeter dmenu
+
+# Software
+pacman -Syu alacritty xterm firefox wireplumber code discord qpwgraph \
+curl ffmpeg filezilla thunderbird gwenview vlc dolphin htop lshw blueman print-manager \
+libreoffice-still okular gnome-screenshot
 
 # setup systemctls
 systemctl enable NetworkManager.service
@@ -12,3 +26,7 @@ greeter-session=lightdm-slick-greeter
 
 # set keyboard layout
 localectl set-x11-keymap de
+
+# Install Rustup
+echo "\n\n\nInstalling Rust\n\n\n"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
